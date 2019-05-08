dd36d9bc3a152c91511cad0fb6f1664ecc442e47
master <qewrqw@dwfds.com>
2019-05-08 19:41:51 +0300

    save task and errors

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,20 +1,34 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
+
 use Illuminate\Http\Request;
+use App\Task;
+
 Route::get('/', function () {
-    return view('tasks.index');//в уроке это вид tasks
+    return view('tasks.index'); //в уроке это вид tasks
 });
 
-Route::post('/task',function(Request $request){
-  
+Route::post('/task', function(Request $request) {
+    $validator = Validator::make($request->all(), [
+                'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/')
+                        ->withInput()
+                        ->withErrors($validator);
+    }
+    $task = new Task();
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
 });
